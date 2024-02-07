4526f144561adc1f28e71d18a78983d13dc70415
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-02-06 17:33:14 -0800

    feat: fading carousel

---
+++ _components/logocarousel/logoCaro.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./logo.module.css";
+import Image from "next/image";
+
+const logos = [
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+  "/pbs.png",
+];
+
+export const Carousel = () => {
+  return (
+    <div className={styles.carousel}>
+      <div className={styles["carousel-inner"]}>
+        {logos.flatMap((logo, i) => [
+          <div key={`logo-${i}`} className={styles.logo}>
+            <Image
+              src={logo}
+              width={240}
+              height={240}
+              alt="News Station Logo"
+            />
+          </div>,
+          <div key={`dup1-${i}`} className={styles.logo}>
+            <Image
+              src={logo}
+              width={240}
+              height={240}
+              alt="News Station Logo"
+            />
+          </div>,
+        ])}
+      </div>
+    </div>
+  );
+};
