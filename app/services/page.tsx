4928d41eb63c0ef5806a045954e73dbe967e388e
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-02-07 17:26:50 -0800

    feat: populate all /service routes with hero

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
-    <div className={styles.page}>
-      <h1>Hello, Service Areas Page!</h1>
-    </div>
+    <>
+      <div className={styles.header}>
+        <h1>Roofing Service Areas</h1>
+      </div>
+      <div className={styles.page}>
+        <div className={styles.hm}>
+          <div className={styles.main}>
+            Grids containing a picture of each location
+            <Link href="/services/ormond">Ormond Beach</Link>
+            <Link href="/services/daytona">Daytona Beach</Link>
+            <Link href="/services/augustine">St. Augustine</Link>
+            <Link href="/services/palmcoast">Palm Coast</Link>
+            <Link href="/services/newsmyrna">New Smyrna Beach</Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
