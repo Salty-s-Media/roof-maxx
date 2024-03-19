5775c278922af607c95a30522425f13ed6a71fc9
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-03-19 16:31:56 -0700

    change: Google Reviews + Logo Clarity + other optims

--- _components/GoogRev/googrev.tsx
+++ _components/GoogRev/googrev.tsx
@@ -2,12 +2,14 @@ import styles from "./goog.module.css";
 
 export const GoogleReviews = () => {
   return (
-    <div className={styles.cont}>
-      <iframe
-        src="https://embedsocial.com/api/pro_hashtag/71ad8ac958dd5cb8c7d917b00752e576ad28e510/?origin=https%3A%2F%2Fwww.roofamp.com%2F"
-        title="EmbedSocial Universal Widget"
-        scrolling="no"
-      ></iframe>
+    <div className={styles.bg}>
+      <h1>Read Our Reviews</h1>
+      <div className={styles.cont}>
+        <iframe
+          src="https://embedsocial.com/api/pro_hashtag/71ad8ac958dd5cb8c7d917b00752e576ad28e510/?origin=https%3A%2F%2Fwww.roofamp.com%2F"
+          title="EmbedSocial Universal Widget"
+        ></iframe>
+      </div>
     </div>
   );
 };
