c5134cd80fa9fcb1f9c38c19d1f39b35cf482853
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-04-30 14:49:20 -0700

    static SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,60 @@ const pjs = Plus_Jakarta_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Roof Maxx",
-  description: "Alternative to costly roof replacement",
+  metadataBase: new URL("https://www.roofamp.com"),
+  keywords: [
+    "roof maxx",
+    "roofing services",
+    "roof repair",
+    "roof replacement",
+    "roofing contractor",
+    "roofing solutions",
+    "roofing repair near me",
+    "best roofing company",
+    "affordable roofing services",
+    "roofing maintenance",
+    "roofing inspection",
+    "roofing repair services",
+    "roofing repair cost",
+    "roofing repair quotes",
+    "roofing repair reviews",
+    "roofing repair tips",
+    "roofing repair FAQ",
+    "roofing repair guide",
+    "roofing repair advice",
+    "roofing repair tips",
+    "roofing repair cost",
+    "roofing repair quotes",
+    "roofing repair reviews",
+    "roofing repair tips",
+    "roofing repair FAQ",
+    "roofing repair guide",
+    "roofing repair advice",
+  ],
+  title: {
+    default: "Roof Amp - Affordable Roofing Solutions",
+    template: "%s | Roof Amp",
+  },
+  description:
+    "Discover Roof Amp's cost effective alternative to roof replacement designed to keep your roof in great condition.",
+  openGraph: {
+    determiner: "auto",
+    title: "Roof Amp - Your Go-To for Affordable Roofing Solutions",
+    description:
+      "Discover Roof Amp's cost effective alternative to roof replacement designed to keep your roof in top condition.",
+    images: [
+      {
+        url: "/logo.svg",
+        width: 305,
+        height: 80,
+        alt: "Roof Amp Logo",
+      },
+    ],
+    url: "https://www.roofamp.com",
+    siteName: "Roof Amp",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
